ad76d32d40e521ac175aa142560d5867dd982cd8
Mattias Sjöström <mattias.sjostrom@seb.se>
2018-05-05 10:40:37 +0200

    lacking duplicate segment check

--- week3/FastCollinearPoints.java
+++ week3/FastCollinearPoints.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
@@ -7,6 +8,7 @@ import edu.princeton.cs.algs4.StdOut;
 public class FastCollinearPoints {
     private int count;
     // private LineSegment[] segments;
+    private ArrayList<LineSegment> seglist;
     
     public FastCollinearPoints(Point[] points) {    // finds all line segments containing 4 or more points
         count = 0;
@@ -19,23 +21,52 @@ public class FastCollinearPoints {
                 if (d != e && points[d].equals(points[e])) throw new java.lang.IllegalArgumentException();
             }
         }
-        Point[] copy = points;
+        Point[] copy = points.clone();
+        Point[] result = new Point[points.length];
+        Double ref;
+        seglist = new ArrayList<LineSegment>();
         for (int i = 0; i < points.length; i++) {
             Arrays.sort(copy, points[i].slopeOrder());
             // StdOut.println(Arrays.toString(copy));
-            for (int j = 0; j < copy.length; j++) {
-                if (points[i].slopeTo(copy[j + 1]) == points[i].slopeTo(copy[j])) {
-                    count++;
-                    
+            int k = 1;
+            ref = Double.NEGATIVE_INFINITY;
+            result[0] = copy[0];
+            for (int j = 1; j < copy.length; j++) {
+                // StdOut.println(points[i].slopeTo(copy[j]) + " " + ref);
+                if (points[i].slopeTo(copy[j]) == ref) {
+                    result[k++] = copy[j];
+                    // StdOut.println("same!\n" + k);
+                }
+                else {
+                    // StdOut.println("new!\n" + k);
+                    if (k >= 3) {
+                        result[k] = points[i];
+                        // StdOut.println("k >= 3, i: " + i + " pre sort result: " + Arrays.toString(result));
+                        Arrays.sort(result, 0, k + 1);
+                        // StdOut.println("k >= 3, i: " + i + " post sort result: " + Arrays.toString(result));
+                        seglist.add(new LineSegment(result[0], result[k]));
+                        StdOut.println((k + 1)+ " point segment for i " + i + ": " + result[0] + "-> " + result[k] + "\n");
+                        count++;
+                        k = 0;
+                    }
+                    else {
+                        // StdOut.println("scrap & reset! " + k + "new ref:" + points[i].slopeTo(copy[j]));
+                        ref = points[i].slopeTo(copy[j]);
+                        k = 0;
+                        result[k++] = copy[j];
+                    }
                 }
             }
-        }      
+        }
     }
     public int numberOfSegments() {       // the number of line segments
         return count;
     }
     public LineSegment[] segments() {               // the line segments
         LineSegment[] segments = new LineSegment[count];
+        for (int i = 0; i < segments.length; i++) {
+            segments[i] = seglist.get(i);
+        }
         return segments;
     }
     public static void main(String[] args) {
